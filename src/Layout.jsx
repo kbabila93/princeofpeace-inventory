982bf86e93dbab5b94e222bbab632ac8c136ee23
k.babila <k.babila@yahoo.com>
2026-01-02 18:53:38 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -84,6 +84,7 @@ export default function Layout({ children, currentPageName }) {
     { name: 'Transactions', href: 'Transactions', icon: History, permission: 'manage_transactions' },
     { name: 'Meetings', href: 'VideoConference', icon: Video, permission: 'view_dashboard' },
     { name: 'Marketing', href: 'AdvertGenerator', icon: Megaphone, permission: 'view_dashboard' },
+    { name: 'Shop Gallery', href: 'ShopGallery', icon: ImageIcon, permission: 'view_dashboard' },
     { name: 'Announcements', href: 'Announcements', icon: Radio, permission: 'view_dashboard' },
     ];
 
